9778321ef4c9c468e7cfa2ee5d1cba4b8f779080
Tanveer Azam Ansari <tnr414@gmail.com>
2023-11-23 13:58:43 +0530

    Add files via upload

---
+++ src/context/GlobalState.js
@@ -0,0 +1,38 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+// intial state
+const intialState = {
+    transactions : [
+
+    ]
+}
+
+//Create Context
+export const GlobalContext = createContext(intialState);
+// Provider Component
+export const GlobalProvider = ({children})=> {
+    const [state, dispatch] = useReducer(AppReducer, intialState);
+
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id,
+        })
+    }
+
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction,
+        })
+    }
+
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+    }}>
+        {children}
+    </GlobalContext.Provider>)
+}
+
